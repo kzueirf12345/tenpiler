b3cbd3bc2d82d1b6155bc6ffd520b71491e7b873
71frukt <71frukt@mail.ru>
2026-02-26 15:35:24 +0300

    fixed bug: add processing onnx_value_info - intermediate graph tensors

--- source/graph/src/GraphOnnxCreator/GraphOnnxCreator.cpp
+++ source/graph/src/GraphOnnxCreator/GraphOnnxCreator.cpp
@@ -12,10 +12,11 @@ namespace graph {
 namespace onnx_parse {
 
 Graph CreateGraph(const onnx::GraphProto& onnx_graph) {
-    const auto& onnx_input    = onnx_graph.input();
-    const auto& onnx_output   = onnx_graph.output();
-    const auto& onnx_internal = onnx_graph.initializer();
-    const auto& onnx_nodes    = onnx_graph.node();
+    const auto& onnx_input      = onnx_graph.input();
+    const auto& onnx_output     = onnx_graph.output();
+    const auto& onnx_internal   = onnx_graph.initializer();
+    const auto& onnx_value_info = onnx_graph.value_info();
+    const auto& onnx_nodes      = onnx_graph.node();
 
     std::unordered_map<std::string, Tensor> tensors;
     
@@ -26,7 +27,7 @@ Graph CreateGraph(const onnx::GraphProto& onnx_graph) {
 
     input  .reserve(onnx_input .size());
     output .reserve(onnx_output.size());
-    tensors.reserve(onnx_input .size() + onnx_output.size() + onnx_internal.size());
+    tensors.reserve(onnx_input .size() + onnx_output.size() + onnx_internal.size() + onnx_value_info.size());
     nodes  .reserve(onnx_nodes .size());
 
     for (const auto& onnx_input_tensor: onnx_input)
@@ -52,8 +53,24 @@ Graph CreateGraph(const onnx::GraphProto& onnx_graph) {
         tensors.emplace(std::move(internal_tensor_name), std::move(internal_tensor));
     }
 
-    for (const auto& onnx_node: onnx_nodes) {
+    for (const auto& onnx_val_info: onnx_value_info)
+    {
+        Tensor val_tensor = onnx_parse::CreateTensor(onnx_val_info);
+        std::string val_tensor_name(val_tensor.name);
+        tensors.emplace(std::move(val_tensor_name), std::move(val_tensor));
+    }
+
+for (const auto& onnx_node: onnx_nodes) {
         nodes.push_back(onnx_parse::NodeFactory::Create(onnx_node));
+
+        for (const auto& out_name : onnx_node.output()) {
+            if (tensors.find(out_name) == tensors.end()) {
+                Tensor dummy_tensor;
+                dummy_tensor.name = out_name;
+                dummy_tensor.type = Tensor::Type::Unknown;
+                tensors.emplace(out_name, std::move(dummy_tensor));
+            }
+        }
     }
 
     return Graph(std::move(tensors), std::move(nodes), std::move(input), std::move(output));
